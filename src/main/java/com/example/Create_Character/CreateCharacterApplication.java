f0b5279488a6a94fd0cf987df77e7bcc7c73db50
Alisha <ashamsher2002@gmail.com>
2026-06-03 11:40:34 +0100

    add first file

---
+++ src/main/java/com/example/Create_Character/CreateCharacterApplication.java
@@ -0,0 +1,13 @@
+package com.example.Create_Character;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreateCharacterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreateCharacterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Create_Character/CreateCharacterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Create_Character;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreateCharacterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
