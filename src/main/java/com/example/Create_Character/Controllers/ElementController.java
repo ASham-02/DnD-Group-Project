8d463b003dcf859c744dffeb0a22d6ef3c49a629
Alisha <ashamsher2002@gmail.com>
2026-06-03 15:18:52 +0100

    initial data to ElementController

--- src/main/java/com/example/Create_Character/Controllers/ElementController.java
+++ src/main/java/com/example/Create_Character/Controllers/ElementController.java
@@ -1,4 +1,25 @@
 package com.example.Create_Character.Controllers;
 
+import com.example.Create_Character.services.ElementService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.lang.model.element.Element;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/elements")
 public class ElementController {
+
+    private final ElementService elementService;
+
+    public ElementController(ElementService elementService) {
+        this.elementService = elementService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Element>> getAllElements() {
+        return ResponseEntity.ok(elementService.getAllElements());
+    }
 }
