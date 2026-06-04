6be39617dc29ed42e93f7e72ec12b1f7d293decf
Zethu Nicole Mthethwa <zethunicolemthethwa@MacBookPro.lan>
2026-06-04 11:14:04 +0100

    feat: add CharacterRepo with findByUserId query

---
+++ src/main/java/com/example/Create_Character/DTOs/character/CharacterResponse.java
@@ -0,0 +1,15 @@
+package com.example.Create_Character.DTOs.stat.character;
+
+public class CharacterResponse {
+
+    private Long id;
+    private Long userId;
+    private String name;
+    private int age;
+    private Long classId;
+    private Long elementId;
+    private String bio;
+    private String artworkUrl;
+
+
+}
---
+++ src/main/java/com/example/Create_Character/DTOs/character/CreateCharacterRequest.java
@@ -0,0 +1,14 @@
+package com.example.Create_Character.DTOs.stat.character;
+
+public class CreateCharacterRequest {
+
+    private Long userId;
+    private String name;
+    private int age;
+    private Long classId;
+    private Long elementId;
+    private String bio;
+    private String artworkUrl;
+
+
+}
