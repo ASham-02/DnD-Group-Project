b1d2da5a1182f9cd0d4ad9d909859ff25b3d6919
idil-abdi <edilabdiaziz@gmail.com>
2026-06-04 13:55:25 +0100

    users login and register

---
+++ src/main/java/com/example/Create_Character/Controllers/UsersAuthController.java
@@ -0,0 +1,48 @@
+package com.example.Create_Character.Controllers;
+
+import com.example.Create_Character.DTOs.stat.ApiResponse;
+import com.example.Create_Character.DTOs.stat.LoginRequest;
+import com.example.Create_Character.DTOs.stat.RegisterRequest;
+import com.example.Create_Character.models.User;
+import com.example.Create_Character.repos.UserRepo;
+import com.example.Create_Character.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value = "/api/users/auth")
+public class UsersAuthController {
+    private final UserService userService;
+
+    public UsersAuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping(value = "/register")
+    public ResponseEntity<ApiResponse> register(@RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(userService.register(request));
+    }
+
+    @PostMapping(value = "/login")
+    public ResponseEntity<ApiResponse> login(@RequestBody LoginRequest request) {
+        return ResponseEntity.ok(userService.login(request));
+    }
+
+    @Autowired
+    private UserRepo userRepo;
+
+    // testing if username exist
+    @GetMapping("/test/{username}")
+    public String testUser2(@PathVariable String username) {
+        Optional<User> user = userRepo.findByUsername(username);
+        System.out.println("is user present> " + user.isPresent());
+        if (user.isPresent()) {
+            return  "user Found: " + user.get().toString();
+        } else {
+            return "User Not Found";
+        }
+    }
+}
--- src/main/java/com/example/Create_Character/CreateCharacterApplication.java
+++ src/main/java/com/example/Create_Character/CreateCharacterApplication.java
@@ -1,7 +1,12 @@
 package com.example.Create_Character;
 
+import com.example.Create_Character.DTOs.stat.ApiResponse;
+import com.example.Create_Character.DTOs.stat.RegisterRequest;
+import com.example.Create_Character.services.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CreateCharacterApplication {
@@ -10,4 +15,19 @@ public class CreateCharacterApplication {
 		SpringApplication.run(CreateCharacterApplication.class, args);
 	}
 
+	//	Testing UserServices
+	/*@Bean
+		CommandLineRunner test(UserService userService) {
+			return args -> {
+				RegisterRequest request = new RegisterRequest();
+				request.setUsername("JohnMike");
+				request.setEmail("JohnMike@test.com");
+				request.setPassword("12345678");
+
+				ApiResponse res = userService.register(request);
+
+				System.out.println(res.getMessage());
+
+			};
+	}*/
 }
