72271994fc15c73ee6562fc434aee86d7e30d358
Alisha <ashamsher2002@gmail.com>
2026-06-04 10:24:56 +0100

    add create element request

--- src/main/java/com/example/Create_Character/DTOs/element/CreateElementRequest.java
+++ src/main/java/com/example/Create_Character/DTOs/element/CreateElementRequest.java
@@ -1,4 +1,25 @@
 package com.example.Create_Character.DTOs.element;
 
 public class CreateElementRequest {
+
+    private String name;
+    private String nation;
+
+    public CreateElementRequest() {}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNation() {
+        return nation;
+    }
+
+    public void setNation(String nation) {
+        this.nation = nation;
+    }
 }
