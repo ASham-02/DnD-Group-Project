97a55ce2ec7445419761b5f5eb9e022d36e53c47
Zethu Nicole Mthethwa <zethunicolemthethwa@MacBookPro.lan>
2026-06-04 11:17:21 +0100

    feat: add CharacterResponse DTO to handle character output data

--- src/main/java/com/example/Create_Character/DTOs/character/CharacterResponse.java
+++ src/main/java/com/example/Create_Character/DTOs/character/CharacterResponse.java
@@ -1,4 +1,4 @@
-package com.example.Create_Character.DTOs.stat.character;
+package com.example.Create_Character.DTOs.character;
 
 public class CharacterResponse {
 
@@ -12,4 +12,22 @@ public class CharacterResponse {
     private String artworkUrl;
 
 
+    public Long getId() { return id; }
+    public Long getUserId() { return userId; }
+    public String getName() { return name; }
+    public int getAge() { return age; }
+    public Long getClassId() { return classId; }
+    public Long getElementId() { return elementId; }
+    public String getBio() { return bio; }
+    public String getArtworkUrl() { return artworkUrl; }
+
+
+    public void setId(Long id) { this.id = id; }
+    public void setUserId(Long userId) { this.userId = userId; }
+    public void setName(String name) { this.name = name; }
+    public void setAge(int age) { this.age = age; }
+    public void setClassId(Long classId) { this.classId = classId; }
+    public void setElementId(Long elementId) { this.elementId = elementId; }
+    public void setBio(String bio) { this.bio = bio; }
+    public void setArtworkUrl(String artworkUrl) { this.artworkUrl = artworkUrl; }
 }
