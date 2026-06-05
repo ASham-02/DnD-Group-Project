afc042e9804617b3747c2d7c743b88870c244e50
Alisha <ashamsher2002@gmail.com>
2026-06-05 13:54:24 +0100

    change all int for element id to Long

--- src/main/java/com/example/Create_Character/repos/ElementRepo.java
+++ src/main/java/com/example/Create_Character/repos/ElementRepo.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ElementRepo extends JpaRepository<Element, Integer> {
+public interface ElementRepo extends JpaRepository<Element, Long> {
 }
